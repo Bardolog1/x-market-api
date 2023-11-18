95a4741a4dbb0dde96e708af985fede9805af302
libardo.lozanogambas <libardo.lozano.contractor@bbva.com>
2023-11-17 22:23:42 -0500

    creacion de estructura del proyecto y creacion de relaciones JPA

--- src/main/java/com/bardolog1/xmarket/web/controller/HellloWorldController.java
+++ src/main/java/com/bardolog1/xmarket/web/controller/HellloWorldController.java
@@ -1,4 +1,4 @@
-package com.bardolog1.xmarket;
+package com.bardolog1.xmarket.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
